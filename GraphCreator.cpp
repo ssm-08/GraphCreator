14d172d827ce96bbeb89d1f1600bd744f1e6f93b
U-BSD\486754 <486754@SHS-GYBF6X3.beaverton.k12.or.us>
2026-05-05 12:42:48 -0700

    setup

--- GraphCreator.cpp
+++ GraphCreator.cpp
@@ -4,6 +4,8 @@
 
 using namespace std;
 
+int getIndex(vector<char[99]> labels, char label[99]);
+
 void addVertex();
 void addEdge();
 void removeVertex();
@@ -12,15 +14,24 @@ void djikstra();
 
 int main() {
 
+  // Start x Destination Adjaceny matrix
   vector<vector<int>> table = {};
+
+  vector<char[99]> labels = {}; 
   
   bool run = true;
   char input[99] = "";
-
+  char start[99] = "";
+  char end[99] = "";
+  int num = 0;
+  
   const char* ADD = "ADD";
   const char* REM = "REMOVE";
-  const char* CON = "CONNECT";
+  const char* PATH = "PATH";
   const char* QUIT = "QUIT";
+
+  const char* VERTEX = "VERTEX";
+  const char* EDGE = "EDGE";
   
   while (run == true) {
 
@@ -29,10 +40,47 @@ int main() {
     cout << endl;
 
     if (strcmp(input, ADD) == 0) {
+      cout << "VERTEX or EDGE?: ";
+      cin >> input;
+      cout << endl;
+
+      if (strcmp(input, VERTEX) == 0) {
+	cout << "Enter label: ";
+	cin >> input;
+	cout << endl;
+
+	// fx
+      } else if (strcmp(input, EDGE) == 0) {
+	cout << "Enter start label: ";
+	cin >> start;
+	cout << endl;
+
+	cout << "Enter end label: ";
+	cin >> end;
+	cout << endl;
+
+	cout << "Enter weight: ";
+	cin >> num;
+	cout << endl;
+
+	//fx
+      }
     } else if (strcmp(input, REM) == 0) {
-    } else if (strcmp(input, CON) == 0) {
+    } else if (strcmp(input, PATH) == 0) {
     } else if (strcmp(input, QUIT) == 0) {
       run = false;
     }
   }
+}
+
+int getIndex(vector<char[99]> labels, char label[99]) {
+
+  for (int i = 0; i < labels.size(); i++) {
+    if (strcmp(labels.at(i), label) == 0) {
+      return i;
+    }
+  }
+
+  return -1;
+
 }
